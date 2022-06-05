4d5601b0aedcafac7ea91d16e7e1716309a9e738
naeluh <naeluh@gmail.com>
2022-06-05 13:31:36 -0700

    update site

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,24 @@
 import Button from '../components/button.js';
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Image from 'next/image';
 import imgsrc from '../public/images/nestor pintana ref.jpg';
 
 const index = (props) => {
+  const ref = useRef(null);
+  useEffect(() => {
+    ref.current.addEventListener('transitionend', (e) => {
+      console.log('Transition ended');
+      const confettiSettings = { target: e.target };
+      const confetti = new ConfettiGenerator(confettiSettings);
+      confetti.render();
+    });
+
+    return () => confetti.clear();
+  }, []); // add the var dependencies or not
   return (
     <article className='outer-cont'>
       <section className='inner-cont'>
-        <header>
+        <header ref={ref}>
           <h1>world cup hair</h1>
         </header>
         <div className='fill'>
