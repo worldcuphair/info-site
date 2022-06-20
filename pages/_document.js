756a168240abe3fcbcba86f20d6a33ca528aba08
naeluh <naeluh@gmail.com>
2022-06-19 22:19:34 -0700

    fixes

--- pages/_document.js
+++ pages/_document.js
@@ -9,12 +9,10 @@ class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
-          <link rel='preconnect' href='https://fonts.googleapis.com' />
-          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
           <link
             href='https://fonts.googleapis.com/css2?family=Noto+Serif+Display:wght@400;500;600;800&display=swap'
             rel='stylesheet'
-          ></link>
+          />
         </Head>
         <body className='dark'>
           <Main />
